378037b3a4590b78fc83bdf0b5f66bd2cfc63b94
SamCodex01 <txtstring@gmail.com>
2026-01-02 21:33:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/estoqueDeProdutos/estoque_de_produtos_spring_boot/EstoqueDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.estoqueDeProdutos.estoque_de_produtos_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estoqueDeProdutos/estoque_de_produtos_spring_boot/EstoqueDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoqueDeProdutos.estoque_de_produtos_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
